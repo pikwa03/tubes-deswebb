f58dcb4458c8f73a4d64e4bab21cb25269d3a02c
pikwa03 <wfqajriyah@gmail.com>
2024-10-31 17:37:26 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import LandingPage from './components/LandingPage'
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
