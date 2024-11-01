30a7f5ca471f6f2e863edb961a17c2c64027467d
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-11-01 10:53:53 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'light-green': '#D8EFD3',
+        'green-custom': '#95D2B3',
+        'dark-green': '#4A7C2D',
+        'text-gray-light': '#D9D9D9',
+        'bg-green-custom': '#55AD9B'
+      },
+    },
+  },
+  plugins: [],
+};
