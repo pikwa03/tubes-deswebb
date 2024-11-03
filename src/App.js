a4c8947e22a086807163a147eacbf72ab6762b80
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-11-03 16:14:02 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import LandingPage from './components/LandingPage'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Dashboard from './components/Dashboard'; // Import the Dashboard component
+import Contact from './components/Contact';     // Import the Contact component
+import Masuk from './components/Masuk';       // Import the SignIn component
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Define the routes for each component */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/Masuk" element={<Masuk />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
