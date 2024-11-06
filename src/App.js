9ded006bd1e838d40dcd5d80445bccd3e7370fbd
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-11-06 18:52:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import Dashboard from './components/Dashboard'; // Import the Dashboard component
-import Contact from './components/Contact';     // Import the Contact component
-import Masuk from './components/Masuk';       // Import the SignIn component
+import Dashboard from './components/Dashboard';
+import Dashboard1 from './components/Dashboard1';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Register from './components/Register';
+import PaymentDetails from './components/PaymentDetails';
+import PesanTiket from './components/PesanTiket';
 
 function App() {
   return (
@@ -13,8 +17,12 @@ function App() {
           {/* Define the routes for each component */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard1" element={<Dashboard1 />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/Masuk" element={<Masuk />} />
+          <Route path="/login" element={<Login />} /> {/* Menambahkan rute untuk Login */}
+          <Route path="/register" element={<Register />} /> {/* Menambahkan rute untuk Register */}
+          <Route path="/test-payment" element={<PaymentDetails />} />
+          <Route path="/pesan-tiket" element={<PesanTiket />} />
         </Routes>
       </div>
     </Router>
