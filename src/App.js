88688366912770e15540a6f27cdccc84953de762
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-11-17 20:31:27 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import LandingPage from './components/LandingPage'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Dashboard from './components/Dashboard';
+import Dashboard1 from './components/Dashboard1';
+import Login from './components/Login';
+import Register from './components/Register';
+import PaymentDetails from './components/PaymentDetails';
+import PesanTiket from './components/PesanTiket';
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Define the routes for each component */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard1" element={<Dashboard1 />} />
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/register" element={<Register />} /> 
+          <Route path="/test-payment" element={<PaymentDetails />} />
+          <Route path="/pesan-tiket" element={<PesanTiket />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
