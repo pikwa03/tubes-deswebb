603672658df0e0a79bd03f707d9db97ec2f22ab9
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-11-30 22:18:19 +0800

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Cek status login dari localStorage
     const loggedInStatus = localStorage.getItem('isLoggedIn') === 'true';
     setIsLoggedIn(loggedInStatus);
-  }, []); // Pastikan tidak ada ketergantungan yang salah di sini
+  }, []);
 
   const handleDropdownToggle = () => {
     setIsDropdownOpen((prev) => !prev);
   };
 
   const handleLogout = () => {
-    // Fungsi logout
+    // Hapus status login di localStorage
     localStorage.setItem('isLoggedIn', 'false');
     setIsLoggedIn(false);
+    // Arahkan pengguna ke halaman utama
+    navigate('/');
   };
 
   return (
     <header className="bg-green p-4 flex justify-between items-center text-white w-full h-[110px]">
       {/* Logo dan Judul */}
       <div className="flex items-center space-x-2">
-        <img src="/images/logo.png" alt="logo" className="w-10 h-10" />
+        <Link to="/">
+          <img src="/images/logo.png" alt="logo" className="w-10 h-10" />
+        </Link>
         <span className="font-bold text-xl leading-none self-center">CHU CHU RAWR</span>
       </div>
 
       {/* Header untuk Landing Page */}
       {location.pathname === '/' && (
         <nav className="font-poppins flex items-center space-x-4 ml-auto">
-          {/* Tombol Masuk */}
+          <Link to="/" className="text-[20px] hover:underline">
+            Home
+          </Link>
+          <Link to="/" className="text-[20px] hover:underline">
+            Logout
+          </Link>
           <div className="relative">
             <button
               onClick={handleDropdownToggle}
@@ -63,9 +72,21 @@ function Header() {
         </nav>
       )}
 
-      {/* Header untuk Halaman Selain Landing Page (Misalnya: Dashboard) */}
+      {/* Header untuk Halaman Selain Landing Page */}
       {location.pathname !== '/' && (
         <nav className="font-poppins flex items-center space-x-4 ml-auto">
+          <Link to="/" className="text-[20px] hover:underline">
+            Home
+          </Link>
+          
+          {isLoggedIn && (
+            <button
+              onClick={handleLogout}
+              className="text-[20px] hover:underline"
+            >
+              Logout
+            </button>
+          )}
           <Link to="/profil1">
             <img
               src="/images/profile.png"
