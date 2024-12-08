255e8de63a87bef85fa04e7bec21128488a15b1e
Dilla <122979935+Dillaayh@users.noreply.github.com>
2024-12-08 22:04:14 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,14 @@ module.exports = {
       colors: {
         'light-green': '#D8EFD3',
         'green-custom': '#95D2B3',
-        'dark-green': '#4A7C2D',
-        'text-gray-light': '#D9D9D9',
-        'green': '#55AD9B'
-      },
+        'dark-green': '#F1F8E8',
+        'text-gray': '#D7F9E6',
+        'green': '#55AD9B',
+        'green-bue': '#46a088',
+        'red': '#8b0000',
+        'hijau' : "#7AC2B4"
+      }
+      
     },
   },
   plugins: [],
